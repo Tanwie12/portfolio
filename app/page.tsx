5d5cda07f655bf1d5754c381bcce10daf86df9e3
Tanwie12 <adeytanwie@gmail.com>
2024-08-27 08:23:44 +0400

    bot

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-
 import Navbar from "@/components/Navbar";
 import HeroSection from "@/components/Hero";
 import About from "@/components/About";
@@ -9,18 +8,21 @@ import Testimonials from "@/components/testimonial/Testimonials";
 import ContactMe from "@/components/ContactMe";
 import Footer from "@/components/Footer";
 import Image from "next/image";
-
+import Bot from "@/components/Bot";
+import Dot from "@/components/Dot";
 
 export default function Home() {
- 
   return (
     <>
       <main className="flex min-h-screen scroll-smooth flex-col justify-center relative md:p-24 p-8 md:pt-32">
-<div className="glow absolute  max-md:hidden -top-20 size-2 -left-20"></div>
+        <div className="glow absolute max-md:hidden -top-20 size-2 -left-20"></div>
 
+        {/* Position the bot to the right bottom corner */}
+        <div className="fixed  left-0 bottom-4 z-50">
+          <Bot />
+        </div>
 
-        <Navbar/>
-
+        <Navbar />
         <HeroSection />
         <About />
         <Skills />
--- components/Dot.tsx
+++ components/Dot.tsx
@@ -1,10 +1,41 @@
-import React from 'react'
+'use client'
+import { Webchat, WebchatProvider, Fab, getClient } from "@botpress/webchat";
+import { buildTheme } from "@botpress/webchat-generator";
+import { useState } from "react";
 
+const { theme, style } = buildTheme({
+  themeName: "prism",
+  themeColor: "#634433",
+});
 type Props = {}
-
+//Add your Client ID here ⬇️
+const clientId = "caacdf9d-3f7e-42ee-aa00-ca9e72302313";
 function Dot({}: Props) {
-  return (
-    <div>Dot</div>
+  
+  const client = getClient({ clientId });
+const [isWebchatOpen, setIsWebchatOpen] = useState(false);
+
+const toggleWebchat = () => {
+  setIsWebchatOpen((prevState) => !prevState);
+};
+
+return (
+  <div style={{ width: "100vw", height: "100vh" }}>
+    <style>{style}</style>
+    <WebchatProvider
+      theme={theme}
+      client={client}
+    >
+      <Fab onClick={toggleWebchat} />
+      <div
+        style={{
+          display: isWebchatOpen ? "block" : "none",
+        }}
+      >
+        <Webchat />
+      </div>
+    </WebchatProvider>
+  </div>
   )
 }
 
