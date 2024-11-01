41ab8a34227999b9285e9137d7b3b1c1309fe990
Tanwie12 <adeytanwie@gmail.com>
2024-11-01 08:16:05 +0400

    edited here

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -37,36 +37,47 @@ const HeroSection = () => {
         </h1>
        
           <TypewriterEffect words={words} className='bodyText mb-6'/>
-          
-        <Button radius='sm' variant='bordered' className=' text-custom-yellow bodyText border-custom-yellow'>
+<div className='gap-2 max-md:grid max-md:grid-cols-2 space-x-5'>
+
+         <Link  href={"#contact"} >
+        <Button radius='sm' variant='bordered' className=' text-customYellow bodyText border-customYellow'>
           Contact Me
         </Button>
-        </div>
-        <div className='flex h-[1/2] flex-row'>
-            <div>
-            <Image src='/gg.png'
-            alt='man'
-            width={500}
-            height={500}
-            />
-            </div>
-             <div className=' self-end hidden md:flex flex-col gap-2'>
-         
-         <div className="shrink-0 self-center w-px bg-[color:var(--YellowColor,#FECD7A)] h-[85px]" />
-         <div className='flex flex-col gap-2'>
-          <Github size={24} color="#FECD7A"/>
-          <Instagram size={24} color="#FECD7A"/>
-          <Linkedin size={24} color="#FECD7A"/>
 
-         </div>
-         
-      
-   
-               </div> 
-           
+        </Link>
+        <Button startContent={<DownloadCloudIcon size={20} className='block md:hidden' />} radius='sm' color='primary' variant='shadow' className=' text-customYellow bodyText border-customYellow'>
+         <span className='hidden md:block'>Download</span>
+         <span>CV</span>
+        </Button>
         </div>
+        </div>
+        <div className='self-end hidden md:flex flex-col gap-2'>
+          <div className="shrink-0 self-center w-px bg-[color:var(--YellowColor,#FECD7A)] h-[85px]" />
+          <div className='flex flex-col gap-2'>
+            <Button 
+              isIconOnly 
+              variant='light' 
+              onClick={() => window.open(socialLinks.github, '_blank')}
+            >
+              <Github size={24} color="#FECD7A"/>
+            </Button>
+            <Button 
+              isIconOnly 
+              variant='light' 
+              onClick={() => window.open(socialLinks.instagram, '_blank')}
+            >
+              <Instagram size={24} color="#FECD7A"/>
+            </Button>
+            <Button 
+              isIconOnly 
+              variant='light' 
+              onClick={() => window.open(socialLinks.linkedin, '_blank')}
+            >
+              <Linkedin size={24} color="#FECD7A"/>
+            </Button>
+          </div>
+        </div> 
       
-        
     </section>
   );
 };
